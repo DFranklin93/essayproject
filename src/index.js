5f431d014bd1fa76f4a27651ade24171430355d0
eelcire <eelcire.eelcire@hotmail.com>
2019-09-25 10:41:58 -0500

    1.0.1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Thing = () => {
+    return (
+        <div>
+            <p>Okay lets get started maggots</p>
+        </div>
+    )
+}
+
+
+ReactDOM.render(<Thing />, document.getElementById('app'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
