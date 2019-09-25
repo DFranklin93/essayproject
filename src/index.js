a11d4793865e9889af5a0098c9b0e2ffcab97e7a
eelcire <eelcire.eelcire@hotmail.com>
2019-09-25 11:47:01 -0500

    1.0.4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import Login from './components/Login'
+import AppRouter from './routers/AppRouter';
 
-ReactDOM.render(<Login />, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,19 +6,23 @@ import EditProfile from '../components/EditProfile'
 import Home from '../components/Home'
 import Login from '../components/Login'
 import Profile from '../components/Profile'
+import NotFoundPage from '../components/NotFoundPage'
+import { createBrowserHistory } from 'history'
+
+const history = createBrowserHistory()
 
 const AppRouter = () => (
     <Router history = {history}>
-      <div>            
-          <Switch>
-            <Route path="/" exact = {true} component={Login}/>
-            <Route path = '/home' component = {Home}/>
-            <Route path="/profile" component={Profile}/>
-            <Route path="/edit" component={EditProfile}/>
-            <Route component={NotFoundPage}/>       
-          </Switch>
-      </div>
+        <div>            
+            <Switch>
+                <Route path="/" exact = {true} component={Login}/>
+                <Route path = '/home' component = {Home}/>
+                <Route path="/profile" component={Profile}/>
+                <Route path="/edit" component={EditProfile}/>
+                <Route path = "*" component={NotFoundPage}/>       
+            </Switch>
+        </div>
     </Router>
-  );
+);
   
-  export default AppRouter;
+export default AppRouter;
